01116eea768a686445e3d5d4b18b3c767449b320
SandipBankar6900 <sb4540994@gmail.com>
2023-12-04 18:11:09 +0530

    ndmdmd

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose=require("mongoose");
-require("dotenv").config();
+// require("dotenv").config();
 
 
-const connection=mongoose.connect(process.env.mongoUrl);
+const connection=mongoose.connect("mongodb+srv://sandip:bankar@cluster0.fppuxm3.mongodb.net/Contact-SchedulerApp?retryWrites=true&w=majority");
 
 module.exports={connection}
