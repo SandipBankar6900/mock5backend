f18c92e18816a82bcd1eaeba8b45b9a0d49d3cd5
SandipBankar6900 <sb4540994@gmail.com>
2023-12-04 18:18:09 +0530

    jsksksk

--- db.js
+++ db.js
@@ -2,6 +2,6 @@ const mongoose=require("mongoose");
 // require("dotenv").config();
 
 
-const connection=mongoose.connect("mongodb+srv://sandip:bankar@cluster0.fppuxm3.mongodb.net/Contact-SchedulerApp?retryWrites=true&w=majority");
+const connection=mongoose.connect("mongodb+srv://sandip:bankar@cluster0.fppuxm3.mongodb.net/contactschedulerapp?retryWrites=true&w=majority");
 
 module.exports={connection}
